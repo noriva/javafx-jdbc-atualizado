ce6b942921c839be3d02c6a86d4b0f4d239293a6
Norival Marson Junior <jrmarson.nmj@gmail.com>
2020-08-24 23:07:33 -0300

    About view

---
+++ src/gui/util/Alerts.java
@@ -0,0 +1,18 @@
+package gui.util;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class Alerts {
+	
+	public static void showAlert(String title, String header, String contente, AlertType type) {
+		
+		Alert alert = new Alert(type);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(contente);
+		alert.show();
+		
+	}
+	
+}
